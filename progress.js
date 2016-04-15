aaba8bd5a477298908b1498bc1426add28fe22a6
Michael Stichling <michael@entelect.co.za>
2016-04-15 10:03:33 +0200

    added more progress feedback

--- progress.js
+++ progress.js
@@ -1,7 +1,14 @@
+function init() {
+
+}
 function UnityProgress (dom) {
   this.progress = 0.0;
   this.message = "";
   this.dom = dom;
+
+  createjs.CSSPlugin.install(createjs.Tween);
+  createjs.Ticker.setFPS(60);
+
   var parent = dom.parentNode;
 
   this.SetProgress = function (progress) {
@@ -9,6 +16,7 @@ function UnityProgress (dom) {
       this.progress = progress;
     if (progress == 1) {
       this.SetMessage("Preparing...");
+      //document.getElementById("spinner").style.display = "inherit";
       document.getElementById("bgBar").style.display = "none";
       document.getElementById("progressBar").style.display = "none";
     }
@@ -24,7 +32,8 @@ function UnityProgress (dom) {
   this.Update = function() {
     var length = 200 * Math.min(this.progress, 1);
     bar = document.getElementById("progressBar")
-    bar.style.width = length + "px";
+    createjs.Tween.removeTweens(bar);
+    createjs.Tween.get(bar).to({width: length}, 500, createjs.Ease.sineOut);
     document.getElementById("loadingInfo").innerHTML = this.message;
   }
   this.Update ();
