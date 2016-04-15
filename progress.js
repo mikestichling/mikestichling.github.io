9b85da19df6abf1d79a628fbd9d68e5aa61fd5a3
Michael Stichling <michael@entelect.co.za>
2016-04-15 10:23:50 +0200

    fixed up the spinner style

--- progress.js
+++ progress.js
@@ -16,7 +16,7 @@ function UnityProgress (dom) {
       this.progress = progress;
     if (progress == 1) {
       this.SetMessage("Preparing...");
-      document.getElementById("spinner").style.display = "inherit";
+      document.getElementById("spinner").style.display = "none";
       document.getElementById("bgBar").style.display = "none";
       //document.getElementById("progressBar").style.display = "none";
     }
