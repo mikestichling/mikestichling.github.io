ec64a3e4250f7465264a47fd5bffe68de1f341cc
Mike Stichling <mikestichling@gmail.com>
2020-02-09 18:15:42 +1100

    Added React front end test

---
+++ react/precache-manifest.56d578cb612bb5d8d95fe70f061b1b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50431a6ab32056eda47592b69b7c0312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "155260b8fef7ecaa37dd",
+    "url": "/static/css/main.1a1dd223.chunk.css"
+  },
+  {
+    "revision": "947d4d426c7fa87e968a",
+    "url": "/static/js/2.eb6763bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.eb6763bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155260b8fef7ecaa37dd",
+    "url": "/static/js/main.5b875401.chunk.js"
+  },
+  {
+    "revision": "26ae48caa67e7048840a",
+    "url": "/static/js/runtime-main.1800ac86.js"
+  }
+]);
